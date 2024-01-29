b1ad7f8be07c21b9f5c71a89ef417273706c842b
Widiaksa <widiaksaputra.wip@gmail.com>
2024-01-29 16:12:24 +0800

    config_database_set

--- config/database.php
+++ config/database.php
@@ -43,26 +43,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mariadb' => [
-            'driver' => 'mysql',
-            'url' => env('https://fooselwoodcraftbali.com'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'fooselwo_foosel-backend'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '123456'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('http://localhost'),
